ad35316e98bfde4087cc7ab21244711979558d27
Justin Maddox <51386183+jus7591@users.noreply.github.com>
2024-04-06 00:02:17 -0500

    Update Commit 4/6
    -client wanted a revamp of the entire site
    -deleted all of special-pages.html, specials.css, all the styling left over from those pages
    -added grid system for pictures under the specials section (will add pricing later based on client feedback
    -added an about me section as that was mentioned before

--- dearly-desserted-remastered-edition/script.js
+++ dearly-desserted-remastered-edition/script.js
@@ -38,35 +38,6 @@ var productSwiper = new Swiper('.gallery-row', {
   },
 });
 
-var blogsSwiper = new Swiper('.blogs-row', {
-  spaceBetween: 30,
-  loop: true,
-  centeredSlides: true,
-  autoplay: {
-    delay: 9500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 1,
-    },
-    1024: {
-      slidesPerView: 1,
-    },
-  },
-});
-
 var reviewSwiper = new Swiper('.review-row', {
   spaceBetween: 30,
   loop: true,
